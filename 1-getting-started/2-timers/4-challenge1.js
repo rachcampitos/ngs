db653057e314ee3cbb0852e41002b0e6243e4c57
rachcampitos <raulo_974@hotmail.com>
2019-08-31 12:44:18 -0500

    Solved chellenge timer - challenge 01

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,12 @@
-const theOneFunc = () => {};
 
-setTimeout(theOneFunc, 4 * 1000);
 
 // Hello after 4 seconds
 
 // Hello after 8 seconds
 
 // You can define only ONE function
+const theOneFunc = x =>{
+console.log('Hello after '+x+' seconds');
+};
+setTimeout(theOneFunc, 4*1000,4);
+setTimeout(theOneFunc, 8*1000,8);
