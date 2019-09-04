faaa27f27d1a03a103087963ee55af25851cb521
rachcampitos <raulo_974@hotmail.com>
2019-09-04 18:40:13 -0500

    Challenge8Solved

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+const  sleep = (ms)=>new Promise(resolve=>(setTimeout(resolve,ms)))
+
+async function demo(){
+    for(i = 0;i<5;i++){
+        console.log("Hello World")
+        await sleep(1000)
+    }
+    console.log("Done");
+    
+}
+demo()
